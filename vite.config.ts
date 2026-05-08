7d6d20e866705ad4cb2142c45fe28a1dcbd00ed9
grilorenato100 <rpn71499@gmail.com>
2026-05-08 10:19:20 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,11 @@ export default defineConfig({
   plugins: [react()],
   base: '/visionpath/',
 })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/visionpath/',
+})
